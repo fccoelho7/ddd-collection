4d35042fd01ca555cd7ecd881765c3cf4f0a1815
Fabio <fccoelho7@gmail.com>
2023-10-17 11:03:42 -0300

    docs: update

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,26 @@ import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>DDD Repository</span>,
+  logo: (
+    <span>
+      <strong>DDD</strong> <small>[Collection]</small>
+    </span>
+  ),
   project: {
-    link: "https://github.com/fccoelho7/ddd-repository",
+    link: "https://github.com/fccoelho7/ddd-collection",
   },
   // chat: {
   //   link: "https://discord.com",
   // },
-  docsRepositoryBase: "https://github.com/fccoelho7/ddd-repository",
+  docsRepositoryBase: "https://github.com/fccoelho7/ddd-collection",
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="DDD Collection" />
+    </>
+  ),
   footer: {
-    text: "DDD Repository",
+    text: "DDD Collection",
   },
 };
 
