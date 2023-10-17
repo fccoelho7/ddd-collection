e41bbbf83720fb342455dca3f7f565d416b8183a
Fabio <fccoelho7@gmail.com>
2023-10-17 14:53:52 -0300

    chore: improve SEO

--- theme.config.tsx
+++ theme.config.tsx
@@ -14,10 +14,19 @@ const config: DocsThemeConfig = {
   //   link: "https://discord.com",
   // },
   docsRepositoryBase: "https://github.com/fccoelho7/ddd-collection",
+  useNextSeoProps() {
+    return {
+      titleTemplate: "%s – DDD Collection",
+    };
+  },
   head: (
     <>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta property="og:title" content="DDD Collection" />
+      <meta
+        property="og:description"
+        content="A collection of practical and illustrative examples that demonstrate how to apply tactical DDD principles"
+      />
     </>
   ),
   footer: {
